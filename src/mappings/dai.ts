4b7a9e73c0cfab92c3ef7bee2d2721c69bec2894
Sergi Juanati <wint3rmute@gmail.com>
2022-09-16 10:19:55 +0200

    Added stablecoin approval mappings

---
+++ src/mappings/dai.ts
@@ -0,0 +1,12 @@
+import { Approval as DaiApprovalEvent } from '../../generated/Dai/Dai';
+import { parseDaiApprovalEvent } from '../parsers/approval';
+import { manageApproval } from '../managers/approvals';
+import { DEPOSIT_HANDLER_ADDRESSES } from '../utils/constants';
+
+
+export function handleApproval(event: DaiApprovalEvent): void {
+  if (DEPOSIT_HANDLER_ADDRESSES.includes(event.params.guy)) {
+    const ev = parseDaiApprovalEvent(event);
+    manageApproval(ev, 'dai');
+  }
+}
