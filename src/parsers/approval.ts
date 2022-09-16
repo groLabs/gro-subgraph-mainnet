58291e65e7f88c23cea48a1e6beb9ba5184a4e26
Sergi Juanati <wint3rmute@gmail.com>
2022-09-16 10:21:51 +0200

    Updated approval parser for USDC & USDT (1e6)

--- src/parsers/approval.ts
+++ src/parsers/approval.ts
@@ -15,6 +15,20 @@ function parseApprovalEvent<T>(ev: T): ApprovalEvent {
     return event;
 }
 
+function parseDaiApprovalEvent<T>(ev: T): ApprovalEvent {
+    const event = new ApprovalEvent(
+        ev.transaction.hash.toHex() + "-" + ev.logIndex.toString(),
+        ev.block.number,
+        ev.block.timestamp,
+        ev.address,
+        ev.params.src.toHexString(),  // links with User.id
+        ev.params.guy,
+        ev.params.wad,
+    )
+    return event;
+}
+
 export {
-    parseApprovalEvent
+    parseApprovalEvent,
+    parseDaiApprovalEvent,
 }
--- src/setters/approvals.ts
+++ src/setters/approvals.ts
@@ -12,9 +12,13 @@ export const setApprovalTx = (
     token: string,
 ): ApprovalTx => {
     let tx = new ApprovalTx(ev.id);
-    const coinAmount = tokenToDecimal(ev.value, 18, 7);
+
+    const base = (token === 'usdc' || token === 'usdt')
+        ? 6
+        : 18;
+    const coinAmount = tokenToDecimal(ev.value, base, 7);
     const pricePerShare = getPricePerShare(token);
-    
+
     tx.ownerAddress = ev.ownerAddress;
     tx.contractAddress = ev.contractAddress;
     tx.block = ev.block.toI32();
