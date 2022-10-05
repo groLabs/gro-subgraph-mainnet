671bf44fa6c95785c6a3be9eedead5f95735a7dc
Sergi Juanati <wint3rmute@gmail.com>
2022-10-05 10:50:50 +0200

    Fixed checking reverted values before accessing value

--- src/utils/pool.ts
+++ src/utils/pool.ts
@@ -54,50 +54,61 @@ export const getUniV2Price = (
     poolAddress: Address,
     update: boolean,
 ): BigDecimal => {
-    // if (reserves.reverted) {
-    //     log.error('getUniV2Price() reverted in src/utils/tokens.ts -> try_getReserves', []);
-    //     return ZERO;
-    // } else if (totalSupply.reverted) {
-    //     log.error('getUniV2Price() reverted in src/utils/tokens.ts -> try_totalSupply', []);
-    //     return ZERO;
-    // } else {
     if (poolAddress == UNISWAPV2_GVT_GRO_ADDRESS) {
         const contract = UniswapV2PairGvtGro.bind(poolAddress);
         const reserves = contract.try_getReserves();
         const totalSupply = contract.try_totalSupply();
-        const total_supply = tokenToDecimal(totalSupply.value, 18, 12);
-        const gvt_reserve = tokenToDecimal(reserves.value.get_reserve0(), 18, 7);
-        const gro_reserve = tokenToDecimal(reserves.value.get_reserve1(), 18, 7);
-        // update Pool data
-        if (update)
-            updatePoolData(
-                1,
-                poolAddress.toHexString(),
-                gvt_reserve,
-                gro_reserve,
-                total_supply,
-            );
-        return ZERO;
+        if (reserves.reverted) {
+            log.error('getUniV2Price() reverted in src/utils/tokens.ts -> try_getReserves on GvtGro pool', []);
+            return ZERO;
+        } else if (totalSupply.reverted) {
+            log.error('getUniV2Price() reverted in src/utils/tokens.ts -> try_totalSupply  on GvtGro pool', []);
+            return ZERO;
+        } else {
+            // update Pool data
+            if (update) {
+                const total_supply = tokenToDecimal(totalSupply.value, 18, 12);
+                const gvt_reserve = tokenToDecimal(reserves.value.get_reserve0(), 18, 7);
+                const gro_reserve = tokenToDecimal(reserves.value.get_reserve1(), 18, 7);
+                updatePoolData(
+                    1,
+                    poolAddress.toHexString(),
+                    gvt_reserve,
+                    gro_reserve,
+                    total_supply,
+                );
+            }
+
+            return ZERO;
+        }
     } else if (poolAddress == UNISWAPV2_GRO_USDC_ADDRESS) {
         const contract = UniswapV2PairGroUsdc.bind(poolAddress);
         const reserves = contract.try_getReserves();
         const totalSupply = contract.try_totalSupply();
-        const total_supply = tokenToDecimal(totalSupply.value, 18, 12);
-        const gro_reserve = tokenToDecimal(reserves.value.get_reserve0(), 18, 7);
-        const usdc_reserve = tokenToDecimal(reserves.value.get_reserve1(), 6, 7);
-        // update Pool data
-        if (update)
-            updatePoolData(
-                2,
-                poolAddress.toHexString(),
-                gro_reserve,
-                usdc_reserve,
-                total_supply,
-            );
-        // return GRO price per share
-        // TODO: chainlink to calc the USD price of USDC.
-        const pps = usdc_reserve.div(gro_reserve).truncate(DECIMALS);
-        return pps;
+        if (reserves.reverted) {
+            log.error('getUniV2Price() reverted in src/utils/tokens.ts -> try_getReserves on GroUsdc pool', []);
+            return ZERO;
+        } else if (totalSupply.reverted) {
+            log.error('getUniV2Price() reverted in src/utils/tokens.ts -> try_totalSupply on GroUsdc pool', []);
+            return ZERO;
+        } else {
+            const total_supply = tokenToDecimal(totalSupply.value, 18, 12);
+            const gro_reserve = tokenToDecimal(reserves.value.get_reserve0(), 18, 7);
+            const usdc_reserve = tokenToDecimal(reserves.value.get_reserve1(), 6, 7);
+            // update Pool data
+            if (update)
+                updatePoolData(
+                    2,
+                    poolAddress.toHexString(),
+                    gro_reserve,
+                    usdc_reserve,
+                    total_supply,
+                );
+            // return GRO price per share
+            // TODO: chainlink to calc the USD price of USDC.
+            const pps = usdc_reserve.div(gro_reserve).truncate(DECIMALS);
+            return pps;
+        }
 
     } else if (poolAddress == UNISWAPV2_USDC_WETH_ADDRESS) {
         // const contract = UniswapV2Pair.bind(poolAddress);
@@ -116,44 +127,3 @@ export const getUniV2Price = (
     }
     // }
 }
-
-
-// export const getUniV2Price = (poolAddress: Address): BigDecimal => {
-//     const contract = UniswapV2Pair.bind(poolAddress);
-//     const reserves = contract.try_getReserves();
-//     const totalSupply = contract.try_totalSupply();
-//     if (reserves.reverted) {
-//         log.error('getUniV2Price() reverted in src/utils/tokens.ts -> try_getReserves', []);
-//         return ZERO;
-//     } else if (totalSupply.reverted) {
-//         log.error('getUniV2Price() reverted in src/utils/tokens.ts -> try_totalSupply', []);
-//         return ZERO;
-//     } else {
-//         if (poolAddress == UNISWAPV2_GRO_USDC_ADDRESS) {
-//             const total_supply = tokenToDecimal(totalSupply.value, 18, 12);
-//             const gro_reserve = tokenToDecimal(reserves.value.get_reserve0(), 18, 7);
-//             const usdc_reserve = tokenToDecimal(reserves.value.get_reserve1(), 6, 7);
-//             // update Pool data
-//             updatePoolData(
-//                 2,
-//                 UNISWAPV2_GRO_USDC_ADDRESS.toHexString(),
-//                 usdc_reserve,
-//                 gro_reserve,
-//                 total_supply,
-//             );
-//             // return GRO price per share
-//             // TODO: chainlink to calc the USD price of USDC.
-//             const pps = usdc_reserve.div(gro_reserve).truncate(DECIMALS);
-//             return pps;
-//         } else if (poolAddress == UNISWAPV2_USDC_WETH_ADDRESS) {
-//             const usdc_reserve = tokenToDecimal(reserves.value.get_reserve0(), 6, 7);
-//             const weth_reserve = tokenToDecimal(reserves.value.get_reserve1(), 18, 7);
-//             // return WETH price per share
-//             // TODO: chainlink to calc the USD price of USDC
-//             const pps = usdc_reserve.div(weth_reserve).truncate(DECIMALS);
-//             return pps;
-//         } else {
-//             return ZERO;
-//         }
-//     }
-// }
