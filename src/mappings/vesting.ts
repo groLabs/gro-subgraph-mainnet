4e008ec3e27fe693505fd0f6cca32dda993907aa
Sergi Juanati <wint3rmute@gmail.com>
2022-10-13 00:01:51 +0200

    Added GROVesting contracts

---
+++ src/mappings/vesting.ts
@@ -0,0 +1,21 @@
+import { LogVest as LogVestV1 } from '../../generated/GROVestingV1/GROVesting';
+import { LogVest as LogVestV2 } from '../../generated/GROVestingV2/GROVesting';
+import { LogExit as LogExitV1 } from '../../generated/GROVestingV1/GROVesting';
+import { LogExit as LogExitV2 } from '../../generated/GROVestingV2/GROVesting';
+
+
+export function handleVestV1(event: LogVestV1): void {
+    //do something
+}
+
+export function handleVestV2(event: LogVestV2): void {
+    // do something
+}
+
+export function handleExitV1(event: LogExitV1): void {
+    //do something
+}
+
+export function handleExitV2(event: LogExitV2): void {
+    // do something
+}
