2b1b8f90c7e4477ced376b154fa9e519c76ac95a
Sergi Juanati <wint3rmute@gmail.com>
2022-11-07 10:47:35 +0100

    Added convex strategies

--- src/mappings/convexStrategies.ts
+++ src/mappings/convexStrategies.ts
@@ -1,5 +1,55 @@
-import { EmergencyExitEnabled } from '../../generated/ConvexStrategy/StableConvexXPool';
+import { DECIMALS } from '../utils/constants';
+import { setHarvest } from '../setters/strats';
+import { tokenToDecimal } from '../utils/tokens';
+import { Harvested as HarvestedPrimaryDAI } from '../../generated/PrimaryStratDAI/StableConvexXPool';
+import { Harvested as HarvestedSecondaryDAI } from '../../generated/SecondaryStratDAI/StableConvexXPool';
+import { Harvested as HarvestedPrimaryUSDC } from '../../generated/PrimaryStratUSDC/StableConvexXPool';
+import { Harvested as HarvestedSecondaryUSDC } from '../../generated/SecondaryStratUSDC/StableConvexXPool';
+import { Harvested as HarvestedPrimaryUSDT } from '../../generated/PrimaryStratUSDT/StableConvexXPool';
 
-export function handleEmergencyExitEnabled(event: EmergencyExitEnabled): void {
-    // do nothing for now
+
+export function handleHarvestedPrimaryDAI(event: HarvestedPrimaryDAI): void {
+    setHarvest(
+        event.address,
+        tokenToDecimal(event.params.profit, 18, DECIMALS),
+        tokenToDecimal(event.params.loss, 18, DECIMALS),
+        event.block.timestamp.toI32(),
+    );
 }
+
+export function handleHarvestedSecondaryDAI(event: HarvestedSecondaryDAI): void {
+    setHarvest(
+        event.address,
+        tokenToDecimal(event.params.profit, 18, DECIMALS),
+        tokenToDecimal(event.params.loss, 18, DECIMALS),
+        event.block.timestamp.toI32(),
+    );
+}
+
+export function handleHarvestedPrimaryUSDC(event: HarvestedPrimaryUSDC): void {
+    setHarvest(
+        event.address,
+        tokenToDecimal(event.params.profit, 6, DECIMALS),
+        tokenToDecimal(event.params.loss, 6, DECIMALS),
+        event.block.timestamp.toI32(),
+    );
+}
+
+export function handleHarvestedSecondaryUSDC(event: HarvestedSecondaryUSDC): void {
+    setHarvest(
+        event.address,
+        tokenToDecimal(event.params.profit, 6, DECIMALS),
+        tokenToDecimal(event.params.loss, 6, DECIMALS),
+        event.block.timestamp.toI32(),
+    );
+}
+
+export function handleHarvestedPrimaryUSDT(event: HarvestedPrimaryUSDT): void {
+    setHarvest(
+        event.address,
+        tokenToDecimal(event.params.profit, 6, DECIMALS),
+        tokenToDecimal(event.params.loss, 6, DECIMALS),
+        event.block.timestamp.toI32(),
+    );
+}
+
