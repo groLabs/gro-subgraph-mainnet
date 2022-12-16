78d22d9b89ba297421bfd1503cc2e8a47637f28c
Wei.Wang <wei@57blocks.com>
2022-12-16 20:31:06 +0800

    Update strategy config to add strategy id

--- src/utils/strats.ts
+++ src/utils/strats.ts
@@ -28,6 +28,7 @@ export const getStrategies = (): Strat[] => {
             'frax',
             'convex',
             true,
+            0,
         ),
         new Strat(
             '0x4d5b5376cbcc001bb4f8930208828ab87d121da8',   // strategy
@@ -41,6 +42,7 @@ export const getStrategies = (): Strat[] => {
             'tusd',
             'convex',
             true,
+            0,
         ),
         new Strat(
             '0x8b335d3e266389ae08a2f22b01d33813d40ed8fd',   // strategy
@@ -54,6 +56,7 @@ export const getStrategies = (): Strat[] => {
             'alusd',
             'convex',
             true,
+            0,
         ),
         new Strat(
             '0xd370998b2e7941151e7bb9f6e337a12f337d0682',   // strategy
@@ -67,6 +70,7 @@ export const getStrategies = (): Strat[] => {
             'gusd',
             'convex',
             true,
+            0,
         ),
         new Strat(
             '0xde5a25415c637b52d59ef980b29a5fda8dc3c70b',   // strategy
@@ -80,10 +84,53 @@ export const getStrategies = (): Strat[] => {
             'ousd',
             'convex',
             true,
+            0,
         ),
+        // new Strat(
+        //     '0x2d13826359803522cce7a4cfa2c1b582303dd0b4',   // strategy
+        //     '0xb9d9e972100a1dd01cd441774b45b5821e136043',   // vault
+        //     ADDR.ZERO.toHexString(),   // g2 no adapter again
+        //     'convexFrax',
+        //     'Convex-Frax',
+        //     '3CRVVault',
+        //     '3CRV Vault',
+        //     '3CRV',
+        //     'Frax',
+        //     'convex',
+        //     true,
+        //     0,
+        // ),
+        // new Strat(
+        //     '0xf5c3953ae4639806fcbcc3196f71dd81b0da4348',   // strategy
+        //     '0xb9d9e972100a1dd01cd441774b45b5821e136043',   // vault
+        //     ADDR.ZERO.toHexString(),   // g2 no adapter again
+        //     'convexMim',
+        //     'Convex-Mim',
+        //     '3CRVVault',
+        //     '3CRV Vault',
+        //     '3CRV',
+        //     'Mim',
+        //     'convex',
+        //     true,
+        //     0,
+        // ),
+        // new Strat(
+        //     '0x3a622db2db50f463df562dc5f341545a64c580fc',   // strategy
+        //     '0xb9d9e972100a1dd01cd441774b45b5821e136043',   // vault
+        //     ADDR.ZERO.toHexString(),   // g2 no adapter again
+        //     'convexLusd',
+        //     'Convex-Lusd',
+        //     '3CRVVault',
+        //     '3CRV Vault',
+        //     '3CRV',
+        //     'Lusd',
+        //     'convex',
+        //     true,
+        //     0,
+        // ),
         new Strat(
-            '0x2d13826359803522cce7a4cfa2c1b582303dd0b4',   // strategy
-            '0xb9d9e972100a1dd01cd441774b45b5821e136043',   // vault
+            '0xd1B9aF64Ed5CdcaEb58955d82FB384b3e558Df7B',   // strategy
+            '0xAe013D9bfa88f54A825831f969CB44ee020872d8',   // vault
             ADDR.ZERO.toHexString(),   // g2 no adapter again
             'convexFrax',
             'Convex-Frax',
@@ -93,32 +140,7 @@ export const getStrategies = (): Strat[] => {
             'Frax',
             'convex',
             true,
-        ),
-        new Strat(
-            '0xf5c3953ae4639806fcbcc3196f71dd81b0da4348',   // strategy
-            '0xb9d9e972100a1dd01cd441774b45b5821e136043',   // vault
-            ADDR.ZERO.toHexString(),   // g2 no adapter again
-            'convexMim',
-            'Convex-Mim',
-            '3CRVVault',
-            '3CRV Vault',
-            '3CRV',
-            'Mim',
-            'convex',
-            true,
-        ),
-        new Strat(
-            '0x3a622db2db50f463df562dc5f341545a64c580fc',   // strategy
-            '0xb9d9e972100a1dd01cd441774b45b5821e136043',   // vault
-            ADDR.ZERO.toHexString(),   // g2 no adapter again
-            'convexLusd',
-            'Convex-Lusd',
-            '3CRVVault',
-            '3CRV Vault',
-            '3CRV',
-            'Lusd',
-            'convex',
-            true,
+            1,
         ),
     ];
     return strats;
